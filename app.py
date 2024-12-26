35ace903d5e3749dcb2a37e89153934dda0f875b
Rojas-Edosn <edson.rojas@kiwify.com.br>
2024-12-26 01:25:16 -0300

    atualização

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import os
+from bson import ObjectId
 
 app = Flask(__name__)
 
@@ -15,12 +16,15 @@ def get_data():
         filtro = request.args.get("filter", "{}")
         campos = request.args.get("fields", "{}")
         filtro = eval(filtro) if filtro else {}
-        campos = eval(campos) if campos else {"_id": 0}
+        campos = eval(campos) if campos else {"_id": 1}
         data = list(collection.find(filtro, campos))
+        for item in data:
+            if "_id" in item:
+                item["_id"] = str(item["_id"])
         return jsonify({"status": "sucesso", "data": data}), 200
     except Exception as e:
         return jsonify({"status": "erro", "mensagem": str(e)}), 500
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
